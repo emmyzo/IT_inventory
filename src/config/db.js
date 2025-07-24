b4e7ab062acf3276759a88d1642be7d170f14fb3
emmyzo <emmyzofynest@gmail.com>
2025-07-24 09:11:13 +0100

    port add

--- src/config/db.js
+++ src/config/db.js
@@ -2,6 +2,7 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('railway', 'root', 'mRUhIeUuCLjxUauTUSFyguyRNEZhokwE', {
   host: 'yamabiko.proxy.rlwy.net',
+  port: 57048,
   dialect: 'mysql',
   logging: false,
 });
